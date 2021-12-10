8c0fcdd14dd732ee15d702cb5954b0daa6165928
Hải Nam <64889646+hainamhuflit@users.noreply.github.com>
2021-12-11 00:48:59 +0700

    footer

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,80 @@
+import React from 'react'
+import './footer.scss'
+
+export default function Footer() {
+    return (
+        <div className='footer'>
+           <div className='footer_list'>
+               <div className='footer-item'>
+                    <h5>Get In Touch</h5>
+                    <ul className='item-list'>
+                        <li>About Us</li>
+                        <li>Careers</li>
+                        <li>Press Releases</li>
+                        <li>Blog</li>
+                    </ul>
+               </div>
+
+               <div className='footer-item'>
+                    <h5>Connections</h5>
+                    <ul className='item-list'>
+                        <li>Facebook</li>
+                        <li>Twitter</li>
+                        <li>Instagram</li>
+                        <li>Youtube</li>
+                        <li>LinkedIn</li>
+                    </ul>
+               </div>
+
+               <div className='footer-item'>
+                    <h5>Earnings</h5>
+                    <ul className='item-list'>
+                        <li>Become an Affiliate</li>
+                        <li>Advertise your product</li>
+                        <li>Sell on Market</li>
+                    </ul>
+               </div>
+
+               <div className='footer-item'>
+                    <h5>Account</h5>
+                    <ul className='item-list'>
+                        <li>Your account</li>
+                        <li>Returns Centre</li>
+                        <li>100% purchase protection</li>
+                        <li>Chat with us</li>
+                        <li>Help</li>
+                    </ul>
+               </div>
+
+           </div>
+           <div className='product-tags'>
+               <h5>Product tags</h5>
+               <div className='tag_list'>
+                    <div className='tag-item'>Beans</div>
+                    <div className='tag-item'>Carrots</div>
+                    <div className='tag-item'>Apples</div>
+                    <div className='tag-item'>Garlic</div>
+                    <div className='tag-item'>Mushrooms</div>
+                    <div className='tag-item'>Tomatoes</div>
+                    <div className='tag-item'>Chilli peppers</div>
+                    <div className='tag-item'>Beans</div>
+                    <div className='tag-item'>Carrots</div>
+                    <div className='tag-item'>Apples</div>
+                    <div className='tag-item'>Garlic</div>
+                    <div className='tag-item'>Mushrooms</div>
+                    <div className='tag-item'>Tomatoes</div>
+                    <div className='tag-item'>Chilli peppers</div>
+                    <div className='tag-item'>Carrots</div>
+                    <div className='tag-item'>Apples</div>
+                    <div className='tag-item'>Garlic</div>
+                    <div className='tag-item'>Mushrooms</div>
+                    <div className='tag-item'>Tomatoes</div>
+                    <div className='tag-item'>Chilli peppers</div>
+               </div>
+
+           </div>
+
+            <span className='coppy-right'>Copyright © 2020 petrbilek.com</span>
+        </div>
+    )
+}
