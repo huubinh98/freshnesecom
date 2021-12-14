b2edc4ed44da232a897e20caf17c7198735b27c2
Hải Nam <64889646+hainamhuflit@users.noreply.github.com>
2021-12-14 17:03:43 +0700

    fix tag

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ import Header from "./components/header";
 
 import "./assets/css/style.scss";
 import HomePage from "./pages/homepage/HomePage";
-
+import Button from "./components/button";
 function App() {
   return (
     <>
       <Header />
       <DropdownMenu />
+      <Button size='medium' color='stroke'>
+        Buy Now
+      </Button>
       <HomePage />
       <Footer />
     </>
--- src/components/footer/Tags.jsx
+++ src/components/footer/Tags.jsx
@@ -4,7 +4,7 @@ export default function Tags({ tag }) {
 
     return (
         <>
-            {tag.map((item) => <div className='tag-item' >{item}</div>)}
+            {tag.map((item) => <div className='tag-item' key={item}>{item}</div>)}
         </>
     )
 }
