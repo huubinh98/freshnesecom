f1cb0f7478e4d63e6f8e55bed4b8db0cab081bf6
Tran Huu Binh <tranhuubinh0198@gmail.com>
2021-12-16 03:30:59 +0700

    update router-dom

--- src/pages/homepage/index.jsx
+++ src/pages/homepage/index.jsx
@@ -1,6 +1,17 @@
 import React from "react";
+import Banner from "./Banner/Banner";
+import BestProduct from "./BestProduct/BestProduct";
+import CategoryMenu from "./CategoryMenu/CategoryMenu";
+import CustomerQuote from "./CustomerQuote/CustomerQuote";
 import "./style.scss";
 
 export default function HomePage() {
-  return <div></div>;
+  return (
+    <div>
+      <CategoryMenu />
+      <Banner />
+      <BestProduct />
+      <CustomerQuote />
+    </div>
+  );
 }
