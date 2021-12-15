a8260ae021bfcd3bd90b934940ce4178e49d87bc
Tran Huu Binh <tranhuubinh0198@gmail.com>
2021-12-16 02:54:39 +0700

    update name component 1

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,7 @@ import Header from "./components/Header";
 
 import "./assets/css/style.scss";
 import HomePage from "./pages/Homepage";
-import Button from "./components/Button";
-import { IconArowBtn } from "./components/Icon";
 import Tabs from "./components/Tabs";
-import SideMenu from "./components/SideMenu";
 
 function App() {
   return (
