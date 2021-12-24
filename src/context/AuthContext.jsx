1436c2f4a066a08506faaaf6b848ff8ade4571ac
Tran Huu Binh <tranhuubinh0198@gmail.com>
2021-12-24 15:40:22 +0700

    fix loi

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,21 +1,14 @@
 const { createContext, useState } = require("react");
 
-const AuthContext = createContext()
-
-
+export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-
   const [login, setLogin] = useState(false);
 
   const [name, setName] = useState("");
   return (
-<<<<<<< HEAD
     <AuthContext.Provider value={{ login, setLogin, name, setName }}>
-=======
-    <AuthContext.Provider>
->>>>>>> master
       {children}
     </AuthContext.Provider>
-  )
-}
+  );
+};
