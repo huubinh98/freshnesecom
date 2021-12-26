6efbafc937496b5b84963c4ca366ba24ee38fbff
Hải Nam <64889646+hainamhuflit@users.noreply.github.com>
2021-12-27 00:51:40 +0700

    add radio component

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import HomePage from "./pages/Homepage";
 import MainLayout from "./layout/MainLayout";
 import "./assets/css/style.scss";
 import NotFound from "./pages/NotFound";
-import CheckoutPage from "./pages/CheckoutPage";
+import CheckoutPage from "./pages/Checkoutpage";
 import LoginPage from "./pages/LoginPage";
 import BlogPage from "./pages/BlogPage";
 import { AuthProvider } from "./context/AuthContext";
--- src/components/Checkbox/index.jsx
+++ src/components/Checkbox/index.jsx
@@ -1,10 +1,14 @@
+import classNames from 'classnames'
 import React from 'react'
 import './style.scss'
 
-const Checkbox = ({ label }) => {
+const Checkbox = ({ label, type }) => {
     return (
-        <div className='squaredcheck'>
-            <input type="checkbox" value="None" id="squaredcheck" class="checkbox1" name="check" />
+        <div className={classNames(
+            'squaredcheck',
+            `type-${type}`
+        )}>
+            <input type="checkbox" value="None" id="squaredcheck" className="checkbox1" name="check" />
             <label htmlFor="squaredcheck"><span>{label}</span></label>
         </div>
     )
