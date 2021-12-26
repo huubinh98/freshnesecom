dd79999e470d5718d6d19fc3c1542bc1a11fa5d8
Hải Nam <64889646+hainamhuflit@users.noreply.github.com>
2021-12-27 01:28:43 +0700

    add bill method component

---
+++ src/pages/Checkoutpage/BillMethod/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { BillMethod2 } from '../../../components/Icon'
+import { BillMethod1 } from '../../../components/Icon'
+import Radio from '../../../components/Radio'
+import './style.scss'
+
+const BillMethod = () => {
+    return (
+        <div className="billing-method">
+            <h2 className='heading'>Billing method</h2>
+
+            <div className="checkout-step">
+                <p className="desc">Please enter your payment method</p>
+                <p className="desc">Step 2 of 5</p>
+            </div>
+            <form>
+                <Radio label='FedEx' price='+32 USD' condition='Additional price' logo={<BillMethod1 />} />
+                <Radio label='DHL' price='+15 USD' condition='Additional price' logo={<BillMethod2 />} />
+            </form>
+        </div>
+
+    )
+}
+
+export default BillMethod
