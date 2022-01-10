c0768441e6bd07e9377e606d3a0cf55223c436f2
Hải Nam <64889646+hainamhuflit@users.noreply.github.com>
2022-01-10 22:46:49 +0700

    fix conflict header info

--- src/store/authenReducer.jsx
+++ src/store/authenReducer.jsx
@@ -6,7 +6,6 @@ const initialState = {
 };
 
 const authenReducer = (state = initialState, action) => {
-<<<<<<< HEAD
     const { type, payload } = action;
     switch (type) {
         case "LOGIN":
@@ -30,33 +29,6 @@ const authenReducer = (state = initialState, action) => {
         default:
             return state;
     }
-=======
-  const { type, payload } = action;
-  switch (type) {
-    case "LOGIN":
-      // handle data
-      localStorage.setItem("token", JSON.stringify(payload));
-      //update state
-      return {
-        login: !!token,
-        loginStatus: true,
-        // user: payload,
-      };
-
-    case "REGIS":
-      return {
-        user: action.payload,
-      };
-    case "LOGOUT":
-      localStorage.removeItem("token");
-      return {
-        login: false,
-        user: null,
-      };
-    default:
-      return state;
-  }
->>>>>>> master
 };
 
 export default authenReducer;
